c15a0548d1aae91e5d44cee152b95551abf1e933
Evgeny Ashurkov <evgeny.biatl0n@gmail.com>
2016-06-05 09:04:20 +0500

    Создал собственное меню

--- wordpress/wp-content/themes/storefrontChild/functions.php
+++ wordpress/wp-content/themes/storefrontChild/functions.php
@@ -1,5 +1,12 @@
 <?php
-/*add_action( 'init', 'remove_widgets' );
+add_action( 'init', 'remove_widgets' );
 function remove_widgets() {
+    remove_action( 'storefront_header', 'storefront_primary_navigation', 50 );
+    remove_action( 'storefront_header', 'storefront_secondary_navigation', 30);
     remove_action( 'storefront_header', 'storefront_product_search', 40 );
-}*/
+    remove_action( 'storefront_header', 'storefront_header_cart', 60 );
+}
+
+if (function_exists('register_nav_menus')) {
+    register_nav_menus(array('main-menu' => 'Main Navigation'));
+}
